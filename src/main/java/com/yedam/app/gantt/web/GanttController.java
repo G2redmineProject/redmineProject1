f64908b5ea8cd1b81d813dd6526eb3c4546df214
sxxmin515 <ksb327d2@gmail.com>
2026-02-04 17:49:20 +0900

    260204_권수민_일정관리list.html수정

--- src/main/java/com/yedam/app/gantt/web/GanttController.java
+++ src/main/java/com/yedam/app/gantt/web/GanttController.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.yedam.app.gantt.service.GanttService;
